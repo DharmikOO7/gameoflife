a85fc6fe3bd72834d9faf5ece0a3795318ebd451
Dharmik Panchal <dharmikp572+github@gmail.com>
2020-09-08 09:46:38 +0530

    Updates

---
+++ precache-manifest.a13f7694198b0e2554bb7515577d2c64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e723667f11e0e92361cecf58632b539",
+    "url": "/gameoflife/index.html"
+  },
+  {
+    "revision": "df58c7e2a694103e5cb6",
+    "url": "/gameoflife/static/css/main.01db7a24.chunk.css"
+  },
+  {
+    "revision": "32f71f188a82d3bdf520",
+    "url": "/gameoflife/static/js/2.e0153bc8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gameoflife/static/js/2.e0153bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df58c7e2a694103e5cb6",
+    "url": "/gameoflife/static/js/main.3c87f4a4.chunk.js"
+  },
+  {
+    "revision": "473159ed8bb1bdd74552",
+    "url": "/gameoflife/static/js/runtime-main.d474d197.js"
+  }
+]);
